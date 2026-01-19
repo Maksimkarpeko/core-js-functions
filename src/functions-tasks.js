6026d39d462db3e2d4f2ff5e7809607408566d26
Maksim <maksim_karpe2ko@mail.ru>
2026-01-19 10:31:28 +0300

    feat:completed getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -51,8 +51,9 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const arrResult = funcs.map((item) => item.length);
+  return arrResult;
 }
 
 /**
